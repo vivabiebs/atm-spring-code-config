2908ff1098f83dfcb315d2c87236e328bc22527b
viva <vivabieber69@gmail.com>
2020-09-14 22:22:14 +0700

    update ATMConfig

--- src/main/java/atm/ATMConfig.java
+++ src/main/java/atm/ATMConfig.java
@@ -3,8 +3,6 @@ package atm;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.File;
-
 @Configuration
 public class ATMConfig {
 
